d09a5817e756525568493d165eb66de69aec62a1
Anton <pozernishku@gmail.com>
2019-06-06 09:15:20 +0300

    Updated spider

--- delivery/spiders/del_sp.py
+++ delivery/spiders/del_sp.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+import hashlib
+import os, json
+import io
+from PIL import Image
+import pytesseract
+from wand.image import Image as wi
+from delivery.read_data import convert_pdf_to_txt, document_to_text
+
 
 class DelSpSpider(scrapy.Spider):
     name = 'del_sp'
     allowed_domains = ['online.hunterexpress.com.au']
-    start_urls = ['http://online.hunterexpress.com.au/']
+    start_urls = ['https://online.hunterexpress.com.au/']
 
     def parse(self, response):
         pass
